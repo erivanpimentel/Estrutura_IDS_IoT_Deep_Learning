358c231ed2660a6e0b904949ffa3a537b6c6d542
Erivan Laranjeira <62409023+erivanpimentel@users.noreply.github.com>
2023-06-04 23:56:36 -0300

    Update classification_IoT.py

--- classification_IoT.py
+++ classification_IoT.py
@@ -1,3 +1,5 @@
+# -->>>> A base de dados Utilizada no modelo está disponivél em: https://www.unb.ca/cic/datasets/iotdataset-2023.html
+
 # Vamos importar alguma bibliotecas
 import pandas as pd
 # Comandos no sistema operacional
